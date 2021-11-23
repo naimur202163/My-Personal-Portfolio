c22b66b7d417d3542864c7a304743e78694187f5
unknown <you@example.com>
2021-11-23 21:17:12 +0600

    my frist portfilio

---
+++ src/PortfolioContainer/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer/Footer';
+import Profile from './Profile/Profile';
+import './Home.css'
+const Home = () => {
+    return (
+        <div className="home-container">
+            <Profile></Profile>
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
