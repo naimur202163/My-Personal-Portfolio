12b8ab9da80e2b8aa6bf4ff1f3f0606de9cbb040
unknown <you@example.com>
2021-11-29 11:49:34 +0600

    email varification

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,124 +1,63 @@
-import React, { useState } from "react";
-import Typical from "react-typical";
-import axios from "axios";
-import { toast } from "react-toastify";
-
-import imgBack from "../../../src/images/mailz.jpeg";
-import load1 from "../../../src/images/load2.gif";
-import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
-import ScrollService from "../../utilities/ScrollService";
-import Animations from "../../utilities/Animations";
-import "./ContactMe.css";
-
-export default function ContactMe(props) {
-  let fadeInScreenHandler = (screen) => {
-    if (screen.fadeScreen !== props.id) return;
-    Animations.animations.fadeInScreen(props.id);
-  };
-
-  const fadeInSubscription =
-    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [banner, setBanner] = useState("");
-  const [bool, setBool] = useState(false);
-
-  const handleName = (e) => {
-    setName(e.target.value);
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const handleMessage = (e) => {
-    setMessage(e.target.value);
-  };
-  console.log(name);
-  const submitForm = async (e) => {
-    e.preventDefault();
-    try {
-      let data = {
-        name,
-        email,
-        message,
-      };
-      setBool(true);
-      const res = await axios.post(`/contact`, data);
-      if (name.length === 0 || email.length === 0 || message.length === 0) {
-        setBanner(res.data.msg);
-        toast.error(res.data.msg);
-        setBool(false);
-      } else if (res.status === 200) {
-        setBanner(res.data.msg);
-        toast.success(res.data.msg);
-        setBool(false);
-
-        setName("");
-        setEmail("");
-        setMessage("");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+const ContactMe = () => {
   return (
-    <div className="main-container" id={props.id || ""}>
-      <ScreenHeading subHeading={"Lets Keep In Touch"} title={"Contact Me"} />
-      <div className="central-form">
-        <div className="col">
-          <h2 className="title">
-            <Typical loop={Infinity} steps={["Get In Touch 📧", 1000]} />
-          </h2>{" "}
-          <a href="#">
-            <i className="fa fa-facebook-square" />
-          </a>
-          <a href="#">
-            <i className="fa fa-google-plus-square" />
-          </a>
-          <a href="#">
-            <i className="fa fa-instagram" />
-          </a>
-          <a href="#">
-            <i className="fa fa-youtube-square" />
-          </a>
-          <a href="#">
-            <i className="fa fa-twitter" />
-          </a>
+
+    <div className="row container">
+      <h2 className="text-muted ">Contact Me</h2>
+      <div className="col-lg-6 col-sm-12 my-5">
+
+        <div  >
+          <img style={{ width: '100%' }} src="https://image.freepik.com/free-photo/communication-connection-speech-bubble-concept_53876-13807.jpg" alt="" />
         </div>
-        <div className="back-form">
-          <div className="img-back">
-            <h4>Send Your Email Here!</h4>
-            <img src={imgBack} alt="image not found" />
-          </div>
-          <form onSubmit={submitForm}>
-            <p>{banner}</p>
-            <label htmlFor="name">Name</label>
-            <input type="text" onChange={handleName} value={name} />
 
-            <label htmlFor="email">Email</label>
-            <input type="email" onChange={handleEmail} value={email} />
-
-            <label htmlFor="message">Message</label>
-            <textarea type="text" onChange={handleMessage} value={message} />
-
-            <div className="send-btn">
-              <button type="submit">
-                send
-                <i className="fa fa-paper-plane" />
-                {bool ? (
-                  <b className="load">
-                    <img src={load1} alt="image not responding" />
-                  </b>
-                ) : (
-                  ""
-                )}
-              </button>
-            </div>
-          </form>
-        </div>
       </div>
+      <div className="col-lg-6 col-sm-12 my-4 me:2">
+
+
+
+
+
+        <Form action="https://formsubmit.co/naimur202163@gmail.com" method="POST" className="">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicText">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="name" placeholder="name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicNumber">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="Number" placeholder="phone Number" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+
     </div>
+
+
   );
-}
+};
+
+export default ContactMe;
+
+
+
+{/* <div>
+      <form action="https://formsubmit.co/naimur202163@gmail.com" method="POST">
+        <input type="text" name="name" required />
+         <input type="email" name="email" required />
+        <button type="submit">Send</button>
+       </form>
+   </div> */}
